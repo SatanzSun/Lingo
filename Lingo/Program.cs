f578ab8de7d0ba63756b0e06b5bc5dda1d9d942e
Reinis Virsnieks <reinis.virsnieks@gmail.com>
2020-11-24 18:34:09 +0200

    kļūdu labojums

--- Lingo/Program.cs
+++ Lingo/Program.cs
@@ -8,7 +8,7 @@ namespace Lingo
             Start:
             string[] wordlist = { "radio", "galds", "piens", "nazis", "pirms", "katls", "tilts", "siena", "laiks", "uguns" };
             Random rnd = new Random();
-            int random = rnd.Next(10);
+            int random = rnd.Next(wordlist.Length);
             string word = wordlist[random];
             string displayWord = word.Substring(0, 1) + "****";
             bool game = true;
@@ -16,26 +16,26 @@ namespace Lingo
             while (game)
             {
                 Dispaly:
-                Console.WriteLine("Jums ir jāmin vārds " + displayWord + ", lai beigtu ievadat 'apnika'.");
+                Console.WriteLine("Jums ir jāmin vārds " + displayWord + ", lai minētu ivadiet vārdu, kas sastāv no pieciem burtiem, lai beigtu ievadat 'apnika'.");
                 string guess = Console.ReadLine();
                 if (string.Equals(guess, end))
                 {
                     game = false;
                     break;
                 }
-                if(guess.Length > 1)
+                if(guess.Length > 5)
                 {
-                    Console.WriteLine("Jūs ievadījāt vairāk par vienu burtu.");
+                    Console.WriteLine("Jūsu ievadītais vārds ir pārāk garš.");
                     goto Dispaly;
                 }
-                for (int i = 1; i < word.Length; i++)
-                {
-                    char character = char.Parse(guess);
-                    if (char.Equals(character, word[i]))
+                    for (int i = 1; i < guess.Length; i++)
                     {
-                        displayWord = displayWord.Remove(i, 1).Insert(i, guess);
+                        if (char.Equals(guess[i], word[i]))
+                        {
+                        string temp = guess[i].ToString();
+                            displayWord = displayWord.Remove(i, 1).Insert(i, temp);
+                        }
                     }
-                }
                 char test = '*';
                 if (!displayWord.Contains(test))
                 {
